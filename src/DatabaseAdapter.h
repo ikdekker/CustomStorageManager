93457ec2adff79afb23c7f8ffe25c74d36e9e724
Nick Dekker <nick@freshcoders.nl>
2016-10-19 16:26:22 +0200

    changed h -> hxx

---
+++ src/DatabaseAdapter.h
@@ -0,0 +1,13 @@
+#include <string>
+#ifndef DATABASEADAPTER_H
+#define DATABASEADATER_H
+
+class DatabaseAdapter {
+public:
+	DatabaseAdapter(string,string,string);
+	
+private:
+	//MYSQL *connection, mysql;
+};
+
+#endif
